4a5a9fec557707af5cbb76ceaeacc64f97026b8b
nvoers <nick-vanoers@hotmail.com>
2022-11-14 16:45:35 +0100

    remove dark

--- ticketdeck/components/navigation.js
+++ ticketdeck/components/navigation.js
@@ -6,19 +6,19 @@ export default function Navigation() {
 
   if (session) {
     return (
-      <nav className="bg-white px-2 py-1 dark:bg-gray-900 sm:px-4">
+      <nav className="bg-white px-2 py-1 sm:px-4">
         <div className="container mx-auto flex flex-wrap items-center justify-between border-b-2 py-3">
           <Link href="/" className="flex items-center no-underline">
-            <span className="self-center whitespace-nowrap text-xl font-semibold text-ticketdeck-blue no-underline dark:text-white">
+            <span className="self-center whitespace-nowrap text-xl font-semibold text-ticketdeck-blue no-underline">
               TicketDeck
             </span>
           </Link>
           <div className="block w-auto">
-            <ul className="mt-0 flex flex-row border-0 bg-white text-sm font-medium dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
+            <ul className="mt-0 flex flex-row border-0 bg-white text-sm font-medium">
               <li>
                 <Link
                   href="/api/auth/signout"
-                  className="block py-2 pr-4 pl-3 text-black no-underline dark:text-white md:p-0"
+                  className="block py-2 pr-4 pl-3 text-black no-underline md:p-0"
                   aria-current="page"
                 >
                   Logout
@@ -31,19 +31,19 @@ export default function Navigation() {
     );
   } else {
     return (
-      <nav className="bg-white px-2 py-1 dark:bg-gray-900 sm:px-4">
+      <nav className="bg-white px-2 py-1 sm:px-4">
         <div className="container mx-auto flex flex-wrap items-center justify-between py-3">
           <Link href="/" className="flex items-center no-underline">
-            <span className="self-center whitespace-nowrap text-xl font-semibold text-ticketdeck-blue no-underline dark:text-white">
+            <span className="self-center whitespace-nowrap text-xl font-semibold text-ticketdeck-blue no-underline ">
               TicketDeck
             </span>
           </Link>
           <div className="block w-auto">
-            <ul className="mt-0 flex flex-row border-0 bg-white text-sm font-medium dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
+            <ul className="mt-0 flex flex-row border-0 bg-white text-sm font-medium ">
               <li>
                 <Link
                   href="/api/auth/signin"
-                  className="block py-2 pr-4 pl-3 text-black no-underline dark:text-white md:p-0"
+                  className="block py-2 pr-4 pl-3 text-black no-underline md:p-0"
                   aria-current="page"
                 >
                   Login
