a4671d525b0d6831232db52c8c13a961bdda434a
nvoers <nick-vanoers@hotmail.com>
2022-12-05 15:13:07 +0100

    database change

--- ticketdeck/prisma/migrations/20221205134035_init/migration.sql
+++ ticketdeck/prisma/migrations/20221205134035_init/migration.sql
@@ -1,9 +1,21 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "ticket" (
     "id" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
     "eventId" TEXT NOT NULL,
     "code" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
 
     CONSTRAINT "ticket_pkey" PRIMARY KEY ("id")
 );
@@ -18,6 +30,12 @@ CREATE TABLE "event" (
 
     CONSTRAINT "event_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- AddForeignKey
 ALTER TABLE "ticket" ADD CONSTRAINT "ticket_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
