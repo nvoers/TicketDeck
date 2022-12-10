737a92c37445be6a3785b565d06f4ca2ccac247d
nvoers <nick-vanoers@hotmail.com>
2022-12-10 18:17:43 +0100

    index page

---
+++ ticketdeck/components/footer.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="w-full bg-white">
+      <div className="container mx-auto py-14">
+        <div className="flex justify-between">
+          <div>
+            <h1 className="mb-1 text-4xl font-bold">TicketDeck</h1>
+            <p className="text-2xl">Personal ticket storage</p>
+          </div>
+          <div className="flex-col">
+            <h1 className="mb-1 text-4xl font-bold">MENU</h1>
+            <ul className="text-2xl">
+              <li>
+                <a href="/api/auth/signin">Register</a>
+              </li>
+              <li>
+                <a href="/api/auth/signin">Login</a>
+              </li>
+              <li>Tickets</li>
+              <li>My profile</li>
+            </ul>
+          </div>
+          <div className="flex-col">
+            <h1 className="mb-1 text-4xl font-bold">CONTACT</h1>
+            <a className="text-2xl">info@nickvanoers.nl</a>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center bg-gray-100 py-3">
+        <a href="https//nickvanoers.nl">
+          Copyright TicketDeck 2022 | Design by Nick van Oers
+        </a>
+      </div>
+    </div>
+  );
+}
--- ticketdeck/tailwind.config.js
+++ ticketdeck/tailwind.config.js
@@ -7,9 +7,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "ticketdeck-blue": "#007bff",
+        "ticketdeck-blue": "#0035B2",
         "ticketdeck-red": "#dc3545",
         "ticketdeck-blue2": "#003bff",
+        "ticketdeck-purple": "#A400B2",
       },
     },
   },
